f5964bf7d08735c272b639e8be3fa591d1961edb
hdnax <hdnax@users.noreply.github.com>
2026-01-25 02:59:01 +0000

    deploy: 0344957ae052f607883499feae74f05b83f3daf9

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-e1539079.js',
+                path_to_root + 'searchindex-ef1b2738.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
